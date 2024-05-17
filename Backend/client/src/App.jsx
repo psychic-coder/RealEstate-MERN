624566341d28cd088831be3aa710ab72ed3a50c4
psychic-coder <rohitganguly450@gmail.com>
2024-05-18 01:40:57 +0530

    Header ui deveopment

--- Backend/client/src/App.jsx
+++ Backend/client/src/App.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 import About from './Pages/About';
+import Home from './Pages/Home';
 import Profile from './Pages/Profile';
 import SignUp from './Pages/SignUp';
 import Signin from './Pages/Signin';
 
 
+
+
 function App() {
   return (
    <BrowserRouter>
+   <Header/>
    <Routes>
-    <Route path='/' element={<Home/>} />
+    <Route path='/' element={<  Home/>} />
     <Route path='/about' element={<About/>} />
     <Route path='/profile' element={<Profile/>} />
     <Route path='/sign-in' element={<Signin/>} />
     <Route path='/sign-up' element={<SignUp/>} />
    </Routes>
+   <Footer/>
    </BrowserRouter>
   )
 }
