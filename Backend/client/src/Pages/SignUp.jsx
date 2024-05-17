8bb2f57540b8b39756c66d9533f67700cfc08307
psychic-coder <rohitganguly450@gmail.com>
2024-05-18 05:26:16 +0530

    complete signup page functionality

--- Backend/client/src/Pages/SignUp.jsx
+++ Backend/client/src/Pages/SignUp.jsx
@@ -1,26 +1,102 @@
-import React from 'react'
-import {Link } from "react-router-dom"
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert } from "flowbite-react";
 function SignUp() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("All fields are required .");
+    }
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data =await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setErrorMessage(data.message);
+        return;
+      }
+
+      setLoading(false);
+      setErrorMessage(null);
+      
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setLoading(false);
+      setErrorMessage(error.message);
+    }
+  };
+
   return (
     <>
-    <div className=" p-3 max-w-lg mx-auto   ">
-    <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-    <form action="" className="flex flex-col gap-4 ">
-      <input type="text" placeholder="username" className="border p-3 rounded-lg" id='username' />
-      <input type="email" placeholder="email" className="border p-3 rounded-lg" id='email' />
-      <input type="password" placeholder="password" className="border p-3 rounded-lg" id='password' />
-      <button  className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80" type='submit'>Sign up</button>
-    </form>
-    <div className='flex gap-2 mt-5 '>
-      <p>Have an account ?</p>
-      <Link to="/sign-in" className="text-blue-700 ">
-        Sign in
-      </Link>
-    </div>
-    </div>
+      <div className=" p-3 max-w-lg mx-auto   ">
+        <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+        <form
+          action=""
+          className="flex flex-col gap-4 "
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            placeholder="username"
+            className="border p-3 rounded-lg"
+            id="username"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            className="border p-3 rounded-lg"
+            id="email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className="border p-3 rounded-lg"
+            id="password"
+            onChange={handleChange}
+          />
+          <button
+            disabled={loading}
+            className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+            type="submit"
+          >
+            {loading ? "loading..." : "Sign Up"}
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5 ">
+          <p>Have an account ?</p>
+          <Link to="/sign-in" className="text-blue-700 ">
+            Sign in
+          </Link>
+        </div>
+        {errorMessage && (
+            <Alert className="mt-5 text-red-600">
+              {errorMessage}
+            </Alert>
+          )}
+      </div>
     </>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
--- Backend/client/vite.config.js
+++ Backend/client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      //we are making a proxy,if we have an api, then it will show the below mentioned rest of the value before as in this case its 'api'
+      //restart the server after making the changes
+      "/api": {
+        target: "http://localhost:5000",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
