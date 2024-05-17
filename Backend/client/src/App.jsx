665230d234a6a8d5a5cdf5fe22c76c6de184e8ba
psychic-coder <rohitganguly450@gmail.com>
2024-05-17 22:29:59 +0530

    Created pages and route

--- Backend/client/src/App.jsx
+++ Backend/client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+import SignUp from './Pages/SignUp';
+import Signin from './Pages/Signin';
+
 
 function App() {
   return (
-    <div>App</div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/profile' element={<Profile/>} />
+    <Route path='/sign-in' element={<Signin/>} />
+    <Route path='/sign-up' element={<SignUp/>} />
+   </Routes>
+   </BrowserRouter>
   )
 }
 
