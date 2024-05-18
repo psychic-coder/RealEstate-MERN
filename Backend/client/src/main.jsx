15c19d10425947e4686a0e09ecac58f2e6dbbf4e
psychic-coder <rohitganguly450@gmail.com>
2024-05-18 20:36:08 +0530

    REDUX PERSISITOR

--- Backend/client/src/main.jsx
+++ Backend/client/src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux';
-import { store } from '../src/redux/store.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "../src/redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- <Provider store={store}>
-  <App/>
- </Provider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
+);
--- Backend/client/src/redux/store.js
+++ Backend/client/src/redux/store.js
@@ -1,16 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from '../redux/user/userSlice';
-//userreducer is the userSlice.reducer we exported from userSlice
+import { configureStore,combineReducers  } from '@reduxjs/toolkit'
+import userReducer from './user/userSlice'
+//in the above line we are importing the userSlice.reducer
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer=combineReducers({
+    user:userReducer,
+})
+
+const persistConfig={
+    key:'root',//name of the key is "root" in the local storage
+    storage,
+    version:1,
+}
+
+const persistedReducer=persistReducer(persistConfig,rootReducer);
 
 
 
 //inplace of adding all the reducers we just added the persistedReducer
 export const store=configureStore({
-    reducer:{user:userReducer},
+    reducer:persistedReducer,
     //the middleware is needed to prevent the error using redux toolkit
     //the serializable check is used to prevent any error in the browser
     middleware:(getDefaultMiddleware)=>
     getDefaultMiddleware({ serializableCheck:false}),
   });
+
+  export const persistor=persistStore(store);
   
   
