e836d9978962040698309f6b84af8f795ff35c2a
psychic-coder <rohitganguly450@gmail.com>
2024-05-19 00:27:59 +0530

    header section update

--- Backend/client/src/App.jsx
+++ Backend/client/src/App.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Pages/About';
-import Home from './Pages/Home';
-import Profile from './Pages/Profile';
-import SignUp from './Pages/SignUp';
-import Signin from './Pages/Signin';
-
-
-
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import SignUp from "./Pages/SignUp";
+import Signin from "./Pages/Signin";
+import PrivateRoute from "./Components/privateRoute";
 
 function App() {
   return (
-   <BrowserRouter>
-   <Header/>
-   <Routes>
-    <Route path='/' element={<  Home/>} />
-    <Route path='/about' element={<About/>} />
-    <Route path='/profile' element={<Profile/>} />
-    <Route path='/sign-in' element={<Signin/>} />
-    <Route path='/sign-up' element={<SignUp/>} />
-   </Routes>
-   <Footer/>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- Backend/client/src/Components/Header.jsx
+++ Backend/client/src/Components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import {useSelector} from "react-redux";
 
 function Header() {
+  const {currentUser}=useSelector((state)=>state.user);
   return (
     <>
       <header className="bg-slate-200 shadow-md ">
@@ -35,10 +37,10 @@ function Header() {
               About
             </li>
             </Link>
-            <Link to="/sign-in">
-            <li className="text-slate-700 hover:underline cursor-pointer">
-              Sign in
-            </li>
+            <Link to="/profile">
+              {currentUser ?(<img className="rounded-full h-7 w-7 object-cover " src={currentUser.avatar} alt="profilePicture"/>):(<li className="text-slate-700 hover:underline cursor-pointer">
+             Sign in
+            </li>)}
             </Link>
           </ul>
         </div>
