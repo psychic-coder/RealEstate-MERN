4281680b7847f04857a9ad735a824194b1ca3038
psychic-coder <rohitganguly450@gmail.com>
2024-05-19 04:27:34 +0530

    create listing api route

--- Backend/api/app.js
+++ Backend/api/app.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 
 mongoose
@@ -27,6 +28,7 @@ app.use(cookieParser());
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+app.use("/api/listing",listingRouter);
 
 
 const port =5000;
