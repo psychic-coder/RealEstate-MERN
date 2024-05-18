29d7c2b1175118a52d8696e921bdf01a6346b559
psychic-coder <rohitganguly450@gmail.com>
2024-05-19 03:40:38 +0530

    add signout functionality

--- Backend/client/src/Pages/Profile.jsx
+++ Backend/client/src/Pages/Profile.jsx
@@ -14,7 +14,10 @@ import {
   updateUserFailure,
   deleteUserStart,
   deleteUserSuccess,
-  deleteUserFailure
+  deleteUserFailure,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess
 } from "../redux/user/userSlice";
 function Profile() {
   const dispatch = useDispatch();
@@ -97,6 +100,22 @@ function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   }
+
+  const handleSignOut=async ()=>{
+      try {
+        dispatch(signOutUserStart());
+        const res=await fetch('/api/auth/signout');
+        const data=await res.json();
+        if(data.success===false){
+          dispatch(signOutUserFailure(data.message));
+          return ;
+        }
+        dispatch(signOutUserSuccess(data));
+      } catch (error) {
+        dispatch(signOutUserFailure(error.message));
+      }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center  my-7 ">Profile</h1>
@@ -160,7 +179,7 @@ function Profile() {
       </form>
       <div className="flex justify-between mt-5 ">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer ">Delete account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5 ">{updateSuccess ? "User is updated successfully !":""}</p>
