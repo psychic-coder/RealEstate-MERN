2a35afa856c2978ab3297b81f76f9ec94f12ed7b
psychic-coder <rohitganguly450@gmail.com>
2024-05-20 08:38:51 +0530

    complete update listing functionality

--- Backend/client/src/App.jsx
+++ Backend/client/src/App.jsx
@@ -9,6 +9,7 @@ import SignUp from "./Pages/SignUp";
 import Signin from "./Pages/Signin";
 import PrivateRoute from "./Components/privateRoute";
 import CreateListing from "./Pages/CreateListing";
+import UpdateListing from "./Pages/UpdateListing";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing/>}/>
+          <Route path="/update-listing/:listingId" element={<UpdateListing/>}/>
         </Route>
 
 
--- Backend/client/src/Pages/Profile.jsx
+++ Backend/client/src/Pages/Profile.jsx
@@ -271,7 +271,10 @@ function Profile() {
                 >
                   Delete
                 </button>
+                <Link to={`/update-listing/${listing._id}`}>
                 <button className="text-green-700 uppercase">Edit</button>
+                </Link>
+                
               </div>
             </div>
           ))}
