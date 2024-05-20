264f2b1ae1e216b771fe15a59fc11277128eb2ec
psychic-coder <rohitganguly450@gmail.com>
2024-05-20 11:47:09 +0530

    complete header search form functionality

--- Backend/client/src/Components/Header.jsx
+++ Backend/client/src/Components/Header.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import {useSelector} from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
-  const {currentUser}=useSelector((state)=>state.user);
+  const navigate = useNavigate();
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*window.location.search is a property in web browsers that returns the query string portion of the current URL.
+The window.location object provides information about the current URL and allows you to navigate to a new URL. The search property specifically returns the query string, which is the part of the URL that comes after the ? character and contains key-value pairs of parameters.*/
+    const urlParams = new URLSearchParams(window.location.search);
+    //the 'searchTerm' is the searchTerm is the key or parameter name that you want to set or update in the query string.
+    urlParams.set("searchTerm", searchTerm);
+    //in the below code we convert the params to string format
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  //If you simply type location.search in the browser's console or in a JavaScript file, it will return the query string portion of the current URL, including the leading ? character.
+  //For example, if the current URL in the browser is https://example.com/search?query=javascript&filter=popular, then location.search would return the following string:
+  //"?query=javascript&filter=popular"
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <>
       <header className="bg-slate-200 shadow-md ">
@@ -16,31 +41,43 @@ function Header() {
             </h1>
           </Link>
           <form
-            action=""
+            onSubmit={handleSubmit}
             className="bg-slate-100 flex items-center  p-3 rounded-lg  "
           >
             <input
               type="text"
               placeholder="Search... "
               className="bg-transparent focus:outline-none w-24 sm:w-64  "
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <FaSearch className="text-slate-600  " />
+            <button>
+              <FaSearch className="text-slate-600  " />
+            </button>
           </form>
           <ul className="flex gap-4 ">
             <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
-              Home
-            </li>
+              <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+                Home
+              </li>
             </Link>
             <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
-              About
-            </li>
+              <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+                About
+              </li>
             </Link>
             <Link to="/profile">
-              {currentUser ?(<img className="rounded-full h-7 w-7 object-cover " src={currentUser.avatar} alt="profilePicture"/>):(<li className="text-slate-700 hover:underline cursor-pointer">
-             Sign in
-            </li>)}
+              {currentUser ? (
+                <img
+                  className="rounded-full h-7 w-7 object-cover "
+                  src={currentUser.avatar}
+                  alt="profilePicture"
+                />
+              ) : (
+                <li className="text-slate-700 hover:underline cursor-pointer">
+                  Sign in
+                </li>
+              )}
             </Link>
           </ul>
         </div>
--- Backend/client/src/Pages/Home.jsx
+++ Backend/client/src/Pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import {Link} from "react-router-dom"
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      {/*top*/}
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl ">
+          Find nour next <span className="text-slate-500 ">perfect</span>
+          <br /> place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm ">
+          Rohit's Estate is the best place to find your new home .<br />
+          We have awide range of properties for you to choose from .
+        </div>
+        <Link className="text-xs sm:text-sm text-blue-800 font-bold hover:underline" to={"/search"}>Let's get started...</Link>
+      </div>
+      {/*swipper*/}
+      
+      {/*listing results for offer, sale and rent*/}
+    </>
+  );
 }
 
-export default Home
+export default Home;
